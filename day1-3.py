9357dce4ad91d9900154a52170a3924679a1d96b
shrutikabirje <64689497+shrutikabirje@users.noreply.github.com>
2020-05-23 18:07:46 +0530

    Add files via upload

---
+++ day1-3.py
@@ -0,0 +1,6 @@
+Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized.
+
+
+a=input("enter the lines you want to capitalize")
+print(a.upper())
+
