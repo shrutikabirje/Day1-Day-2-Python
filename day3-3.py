c574d95812e8f291d4e3b216f8ac4613a4f8d11f
shrutikabirje <64689497+shrutikabirje@users.noreply.github.com>
2020-06-04 11:21:12 +0530

    Update day3-3.py

--- day3-3.py
+++ day3-3.py
@@ -5,11 +5,9 @@
 1 2 3 4
 1 2 3 4 5
 
-
 n=int(input("enter the no."))
-for i in range (0,n+1):
-    n=1
+for i in range(1,n+1):
     for j in range(1,i+1):
-        print(n,end=" ")
-        n=n+1
+        print(j,end=" ")
     print("\n")
+    
